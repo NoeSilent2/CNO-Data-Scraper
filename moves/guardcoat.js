a140b08036681b1746776b16a310623894c4d942
NoeSilent2 <ezekielr2006@gmail.com>
2026-05-28 12:51:46 -0400

    lasers editted + fossils
    reintroduces lasers editted (with some edits) and also adds the changes to fossilmon combinations

--- moves/guardcoat.js
+++ moves/guardcoat.js
@@ -8,6 +8,9 @@
     priority: 0,
     flags: { contact: 1, protect: 1, mirror: 1 },
     overrideOffensiveStat: "def",
+    secondary: {
+      chance: 30,
+    },
     target: "normal",
     type: "Normal"
 }
