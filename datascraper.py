928c2e2b19208004888c7e26530c9fc630b3df3e
NoeSilent2 <ezekielr2006@gmail.com>
2026-07-23 14:18:05 -0400

    regional hisui bias spawn conditions

--- datascraper.py
+++ datascraper.py
@@ -427,7 +427,7 @@ class DataScraper:
 				return "cannot process herd files"
 			# A large ugly stack of replacements for alt_internal_name stuff. This should probably get organized and put into its own function
 			pokemon = spawn.get('pokemon', '').lower().replace(' ', '-').replace('_','-')
-			pokemon = pokemon.replace('gmax', 'gigantamax').replace('region-bias=alola','alolan-bias').replace('region-bias=galar','galarian-bias').replace('-cream','-cream-love').replace('-swirl','-swirl-love').replace('-goth','-midnight')
+			pokemon = pokemon.replace('gmax', 'gigantamax').replace('region-bias=alola','alolan-bias').replace('region-bias=galar','galarian-bias').replace('region-bias=hisui','hisuian-bias').replace('-cream','-cream-love').replace('-swirl','-swirl-love').replace('-goth','-midnight')
 			removals = ["flower=","dance-style=","-amethyst","-emerald","-echo","-quartz","-allay","spell-forme=","'",'\u2019',"cobblemon:"]
 			for removal in removals:
 				pokemon = pokemon.replace(removal,"")
